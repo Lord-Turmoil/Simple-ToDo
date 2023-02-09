978be9b04e4879fcf76100956c90e3811702dd4f
Turmoil <tony-turmoil@qq.com>
2023-02-09 16:48:08 +0800

    basic frame for main window

---
+++ SimpleToDo/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleToDo
+{
+	public class MainViewModel : ObservableObject
+	{
+		public RelayCommand? OnMinimizeButtonClicked { get; }
+		public RelayCommand? OnMaximizeButtonClicked { get; }
+		public RelayCommand? OnRestoreButtonClicked { get; }
+		public RelayCommand? OnCloseButtonClicked { get; }
+
+		public MainViewModel()
+		{
+			OnMinimizeButtonClicked = new RelayCommand(() =>
+			{
+				SystemCommands.MinimizeWindow(Application.Current.MainWindow);
+			});
+
+			OnMaximizeButtonClicked = new RelayCommand(() =>
+			{
+				SystemCommands.MaximizeWindow(Application.Current.MainWindow);
+			});
+
+			OnRestoreButtonClicked = new RelayCommand(() =>
+			{
+				SystemCommands.RestoreWindow(Application.Current.MainWindow);
+			});
+
+			OnCloseButtonClicked = new RelayCommand(() =>
+			{
+				SystemCommands.CloseWindow(Application.Current.MainWindow);
+			});
+		}
+	}
+}
--- SimpleToDo/MainWindow.xaml.cs
+++ SimpleToDo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using Prism.Services.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,32 @@ namespace SimpleToDo
 		public MainWindow()
 		{
 			InitializeComponent();
+			this.MaxHeight = SystemParameters.PrimaryScreenHeight;
+			this.DataContext = new MainViewModel();
+
+			StateChanged += _MainWindowStateChangeRaised;
+
+			colorZone.MouseDown += (s, e) => { this.DragMove(); };
+			colorZone.MouseDoubleClick += (s, e) => { _ChangeMainWindowState(); };
+		}
+
+		private void _MainWindowStateChangeRaised(object? sender, EventArgs e)
+		{
+			_ChangeMainWindowState();
+		}
+
+		private void _ChangeMainWindowState()
+		{
+			if (WindowState == WindowState.Maximized)
+			{
+				btnMaximize.Visibility = Visibility.Collapsed;
+				btnRestore.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				btnMaximize.Visibility = Visibility.Visible;
+				btnRestore.Visibility = Visibility.Collapsed;
+			}
 		}
 	}
 }
