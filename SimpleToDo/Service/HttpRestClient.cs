de5378f6079041f16973b7834305065e68031058
Turmoil <tony-turmoil@qq.com>
2023-02-28 23:27:56 +0800

    add service

---
+++ SimpleToDo/Service/HttpRestClient.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using SimpleToDo.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Service
+{
+	public class HttpRestClient
+	{
+		private readonly string _apiUrl;
+		private readonly RestClient _client;
+
+		public HttpRestClient(string apiUrl)
+		{
+			_apiUrl = apiUrl;
+			var options = new RestClientOptions("")
+			{
+				MaxTimeout = -1
+			};
+			_client = new RestClient(options);
+		}
+
+		public async Task<ApiResponse> ExecuteAsync(BaseRequest request)
+		{
+			var restRequest = new RestRequest(_apiUrl + request.Route, request.Method);
+			restRequest.AddHeader("ContentType", request.ContentType);
+
+			if (request.Parameter != null)
+				restRequest.AddParameter("param", JsonConvert.SerializeObject(request.Parameter));
+			
+			RestResponse response = await _client.ExecuteAsync(restRequest);
+			
+			return JsonConvert.DeserializeObject<ApiResponse>(response.Content);
+		}
+
+	}
+}
