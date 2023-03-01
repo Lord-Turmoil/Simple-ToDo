884bd71cce084f1f123161965ae0064d5ce5dfdd
Turmoil <tony-turmoil@qq.com>
2023-03-01 21:06:17 +0800

    feat: add template response

--- SimpleToDo.Shared/ApiResponse.cs
+++ SimpleToDo.Shared/ApiResponse.cs
@@ -9,21 +9,42 @@ namespace SimpleToDo.Shared
 	public class ApiResponse
 	{
 		public bool Status { get; set; }
-		public object? Result { get; set; }
 		public string? Message { get; set; }
+		public object? Result { get; set; }
 
 		public ApiResponse(string message, bool status = false)
 		{
 			Status = status;
-			Result = null;
 			Message = message;
+			Result = null;
 		}
 
 		public ApiResponse(object result, bool status = true)
 		{
 			Status = status;
-			Result = result;
 			Message = null;
+			Result = result;
+		}
+	}
+
+	public class ApiResponse<TResult> where TResult : class
+	{
+		public bool Status { get; set; }
+		public string? Message { get; set; }
+		public TResult? Result { get; set; }
+
+		public ApiResponse(string message, bool status = false)
+		{
+			Status = status;
+			Message = message;
+			Result = null;
+		}
+
+		public ApiResponse(TResult result, bool status = true)
+		{
+			Status = status;
+			Message = null;
+			Result = result;
 		}
 	}
 }
