73ed3b0b1e18cdc9c0b9ee73e8f536347bc3068c
Turmoil <tony-turmoil@qq.com>
2023-03-01 23:12:15 +0800

    add ToDo service

---
+++ SimpleToDo/Service/IToDoService.cs
@@ -0,0 +1,13 @@
+﻿using SimpleToDo.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Service
+{
+	public interface IToDoService : IBaseService<ToDoDto>
+	{
+	}
+}
---
+++ SimpleToDo/Service/ToDoService.cs
@@ -0,0 +1,16 @@
+﻿using SimpleToDo.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Service
+{
+	public class ToDoService : BaseService<ToDoDto>, IToDoService
+	{
+		public ToDoService(HttpRestClient client) : base(client, "ToDo")
+		{
+		}
+	}
+}
